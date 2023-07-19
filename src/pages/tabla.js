0ac3ace902a6437b34c7017193644b151c353ddf
Mauricio David Castillo Ruvalcaba <maurix.cas.ruv11@gmail.com>
2023-07-18 23:27:27 -0600

    Final version

--- src/pages/tabla.js
+++ src/pages/tabla.js
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Navbar from '../components/navar';
 import "../assets/styles/graficas.css"
 
-const Tablas = () =>{
-
+const Tablas = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -22,86 +21,68 @@ const Tablas = () =>{
       .catch(error => console.log('error', error));
   };
 
-  const sum1 = data.map(item => item.ambientHumidity).reduce((acc, ambientHumidity) => acc + ambientHumidity, 0);
-  const average1 = sum1 / data.length;
-  const squaredDifferencesSum1 = data.map(item => item.ambientHumidity).reduce((acc, ambientHumidity) => acc + Math.pow(ambientHumidity - average1, 2));
-  const variance1 = squaredDifferencesSum1 / (data.length - 1);
-  const standardDeviation1 = Math.sqrt(variance1);
+  // Función reutilizable para calcular la media, varianza y desviación estándar
+  const calculateStats = (data, key) => {
+    if (!data || !data.length) return { average: 0, variance: 0, standardDeviation: 0 };
 
-  const sum2 = data.map(item => item.soilHumidity).reduce((acc, soilHumidity) => acc + soilHumidity, 0);
-  const average2 = sum2 / data.length;
-  const squaredDifferencesSum2 = data.map(item => item.soilHumidity).reduce((acc, soilHumidity) => acc + Math.pow(soilHumidity - average2, 2));
-  const variance2 = squaredDifferencesSum2 / (data.length - 1);
-  const standardDeviation2 = Math.sqrt(variance2);
+    const sum = data.map(item => item[key]).reduce((acc, value) => acc + value, 0);
+    const average = sum / data.length;
+    const squaredDifferencesSum = data.map(item => item[key]).reduce((acc, value) => acc + Math.pow(value - average, 2));
+    const variance = squaredDifferencesSum / (data.length - 1);
+    const standardDeviation = Math.sqrt(variance);
 
-  const sum3 = data.map(item => item.ambientTemperature).reduce((acc, ambientTemperature) => acc + ambientTemperature, 0);
-  const average3 = sum3 / data.length;
-  const squaredDifferencesSum3 = data.map(item => item.ambientTemperature).reduce((acc, ambientTemperature) => acc + Math.pow(ambientTemperature - average3, 2));
-  const variance3 = squaredDifferencesSum3 / (data.length - 1);
-  const standardDeviation3 = Math.sqrt(variance3);
+    return { average, variance, standardDeviation };
+  };
 
-  // Calcula la suma de los valores utilizando el método map() y reduce()
-  const sum = data.map(item => item.luminosity).reduce((acc, luminosity) => acc + luminosity, 0);
-  // Calcula la media dividiendo la suma entre la cantidad de elementos
-  const average = sum / data.length;
-  // Calcula la suma de los cuadrados de las diferencias entre cada dato y la media
-  const squaredDifferencesSum = data.map(item => item.luminosity).reduce((acc, luminosity) => acc + Math.pow(luminosity - average, 2));
-  // Calcula la varianza
-  const variance = squaredDifferencesSum / (data.length - 1);
-  // Calcula la desviación estándar (raíz cuadrada de la varianza)
-  const standardDeviation = Math.sqrt(variance);
-
+  // Calcula las estadísticas para cada variable
+  const { average: avgHumedadAmbiental, variance: varHumedadAmbiental, standardDeviation: stdHumedadAmbiental } = calculateStats(data, 'ambientHumidity');
+  const { average: avgHumedadTierra, variance: varHumedadTierra, standardDeviation: stdHumedadTierra } = calculateStats(data, 'soilHumidity');
+  const { average: avgTemperatura, variance: varTemperatura, standardDeviation: stdTemperatura } = calculateStats(data, 'ambientTemperature');
+  const { average: avgIluminacion, variance: varIluminacion, standardDeviation: stdIluminacion } = calculateStats(data, 'luminosity');
 
   return (
     <div className='page_Principal'>
-      <Navbar/>
+      <Navbar />
       <div className='container-tabla'>
-      <table class="table">
-        <thead>
-          <tr>
-            <th>Humedad ambiental</th>
-            <th>Humedad de la tierra</th>
-            <th>Temperatura</th>
-            <th>Iluminación</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(item => (
-            <tr key={item.id}>
-              <td>{item.ambientHumidity}</td>
-              <td>{item.soilHumidity}</td>
-              <td>{item.ambientTemperature}</td>
-              <td>{item.luminosity}</td>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Humedad ambiental</th>
+              <th>Humedad de la tierra</th>
+              <th>Temperatura</th>
+              <th>Iluminación</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <h4>Media de la Humedad ambiental: </h4>
-      {/*<p>Suma total de los valores: {sum1}</p>*/}
-      <p>Media: {average1}</p>
-      <p>Varianza: {variance1}</p>
-      <p>Desviación Estándar: {standardDeviation1}</p>
-      <h4>Media de la Humedad de la tierra:</h4>
-      {/*<p>Suma total de los valores: {sum2}</p>*/}
-      <p>Media: {average2}</p>
-      <p>Varianza: {variance2}</p>
-      <p>Desviación Estándar: {standardDeviation2}</p>
-      <h4>Media de la Temperatura:</h4>
-      {/*<p>Suma total de los valores: {sum3}</p>*/}
-      <p>Media: {average3}</p>
-      <p>Varianza: {variance3}</p>
-      <p>Desviación Estándar: {standardDeviation3}</p>
-      <h4>Media de la Iluminación:</h4>
-      {/*<p>Suma total de los valores: {sum}</p>*/}
-      <p>Media: {average}</p>
-      <p>Varianza: {variance}</p>
-      <p>Desviación Estándar: {standardDeviation}</p>
+          </thead>
+          <tbody>
+            {data.map(item => (
+              <tr key={item.id}>
+                <td>{item.ambientHumidity}</td>
+                <td>{item.soilHumidity}</td>
+                <td>{item.ambientTemperature}</td>
+                <td>{item.luminosity}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <h4>Media de la Humedad ambiental:</h4>
+        <p>Media: {avgHumedadAmbiental}</p>
+        <p>Varianza: {varHumedadAmbiental}</p>
+        <p>Desviación Estándar: {stdHumedadAmbiental}</p>
+        <h4>Media de la Humedad de la tierra:</h4>
+        <p>Media: {avgHumedadTierra}</p>
+        <p>Varianza: {varHumedadTierra}</p>
+        <p>Desviación Estándar: {stdHumedadTierra}</p>
+        <h4>Media de la Temperatura:</h4>
+        <p>Media: {avgTemperatura}</p>
+        <p>Varianza: {varTemperatura}</p>
+        <p>Desviación Estándar: {stdTemperatura}</p>
+        <h4>Media de la Iluminación:</h4>
+        <p>Media: {avgIluminacion}</p>
+        <p>Varianza: {varIluminacion}</p>
+        <p>Desviación Estándar: {stdIluminacion}</p>
       </div>
     </div>
-    
   );
 };
 
 export default Tablas;
-
-    
